407f8e78c2d5a89552151f922dd1999174253d76
Kit29 <149437558+Kit29@users.noreply.github.com>
2024-02-26 01:32:25 +0200

    goit-js-hw-01
    hw1

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,6 @@
-console.log(Math.floor(1.1)); // 1
-console.log(Math.floor(1.7)); // 1
+function makeTransaction(quantity, pricePerDroid) {
+console.log(`You ordered ${quantity} droids worth ${pricePerDroid} credits!`);
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+}
