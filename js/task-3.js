88c8828f417ddaae76c566b07366ff98c0596dff
Kit29 <149437558+Kit29@users.noreply.github.com>
2024-02-26 12:58:27 +0200

    goit-js-hw-01
    hw01

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,11 @@
 function getElementWidth(content, padding, border) {
-var totalWidth = content + padding * border;
-return totalWidth;
-}
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+    let contentNum = parseFloat(content);
+    let paddingNum = parseFloat(padding);
+    let borderNum = parseFloat(border);
+
+    let totalWidth = contentNum + 2 * paddingNum + 2 * borderNum;
+    return totalWidth;
+  }
+  console.log(getElementWidth("50px", "8px", "4px"));    // 74
+  console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+  console.log(getElementWidth("200px", "0px", "0px"));   // 200
